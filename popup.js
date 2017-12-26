f84857798d1782987030c34b0d5717093fc3cf74
dc788 <dc788@cornell.edu>
2017-12-27 00:02:48 +0800

    options page

--- popup.js
+++ popup.js
@@ -2,9 +2,10 @@
  * Gets the domain name of the URL of the current tab
  * @param {function(string)} callback Called when the URL is found
  */
-function getDomain(callback) {
-
-	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+function getDomain(callback)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs) =>
+	{
 		var tab = tabs[0];
 		var url = new URL(tab.url);
 		callback(url.hostname);
@@ -14,23 +15,36 @@ function getDomain(callback) {
 /**
  * Listen for clicks on blockButton. Once the button is clicked, add the url domain to the stored array.
  */
-document.addEventListener('DOMContentLoaded', () => {
-	getDomain((domain) => {
-		var blockButton = document.getElementById('blockButton');
-		blockButton.addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () =>
+{
+	getDomain((domain) =>
+	{
 
+		//url text
+		var urlText = document.getElementById('urlText');
+		urlText.textContent = domain;
+
+		//block button
+		var blockButton = document.getElementById('blockButton');
+
+		blockButton.addEventListener('click', () =>
+		{
 			//set icon so user knows this website is blocked
-			//NOTE: This code resembles
-			chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-				chrome.browserAction.setIcon({path: {
+			//NOTE: This code resembles setBlocked1(), but without sending messages.
+			chrome.tabs.query({active: true, currentWindow: true}, (tabs) =>
+			{
+				chrome.browserAction.setIcon({
+					path: {
 						"16": "Resources/blocked1-16.png",
 						"48": "Resources/blocked1-48.png",
 						"128": "Resources/blocked1-128.png"
-					}, tabId: tabs[0].id});
+					}, tabId: tabs[0].id
+				});
 			});
 
 			//save the url
-			chrome.storage.sync.get("urls", (items) => {
+			chrome.storage.sync.get("urls", (items) =>
+			{
 				var urls = items.urls;
 
 				//the url array is null
@@ -44,6 +58,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 				chrome.storage.sync.set({"urls": urls});
 			});
+		});
+
+			//options button
+		var optionsButton = document.getElementById("optionsButton");
+		optionsButton.addEventListener('click', () =>
+		{
+			chrome.tabs.create({active: true, url: "options.html"});
 		});
 	});
 });
